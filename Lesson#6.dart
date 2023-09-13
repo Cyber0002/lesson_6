cee83c28f091659c85afccf98f0b2205d43878b7
Баймырза <baimyrza@MacBook-Air-Bajmyrza.local>
2023-09-13 23:44:58 +0600

    add lesson file

---
+++ Lesson#6.dart
@@ -0,0 +1,89 @@
+
+void main() {
+//2
+//   List listOfNumbers = [1, 2, 3, 4, 5, 6];
+
+//   ShowList(listOfNumbers);
+// }
+
+// ShowList(List list) {
+//   print('${list.first} ${list.last}');
+
+// //3
+//   double a = 3;
+//   double b = 4;
+
+//   double hypotenuse = (a * a + b * b);
+
+//   print('Гипотенуза = $hypotenuse');
+
+//4
+//     int a = 4;
+//     int b = 5;
+
+//     DrawSquare(a, b);
+
+// }
+
+// DrawSquare(int height, int width) {
+//     for (int i = 0; i < height; i++) {
+//       print('*' * width);
+//     }
+//   }
+
+//5
+  // int a = 8;
+  // DrawPyramid(a);
+//}
+
+// DrawPyramid(int height) {
+//   String zvezda = "*";
+
+//   for (int i = height - 1; i >= 0; i--) {
+//     print(' ' * i + zvezda);
+//     zvezda += "**";
+//   }
+// }
+
+
+//6
+//   String stroka = '123456';
+//   IsSumEquals(stroka);
+// }
+
+// IsSumEquals(String stroka) {
+//   String first = stroka.substring(0,3);
+//   String second = stroka.substring(3,6);
+//   int firstSum = int.parse(first[0]) + int.parse(first[1]) + int.parse(first[2]);
+//   int secondSum = int.parse(second[0]) + int.parse(second[1]) + int.parse(second[2]);
+
+//   if (firstSum == secondSum) print("да");
+//   else print("нет");
+// }
+
+
+//7
+// String str = '12345';
+
+// IsNumstartWith(str);
+// }
+
+// IsNumstartWith(String str){
+//   if (str.startsWith('1')|| str.startsWith('2') || str.startsWith('3')) {
+//     print('Да');
+//   } else {
+//     print('Нет');
+//   }
+// }
+
+// String str = 'abcde';
+
+// IsNumstartWith(str);
+// }
+
+// IsNumstartWith(String str){
+//   if (str.startsWith('a')) {
+//     print('Да');
+//   }
+
+}
